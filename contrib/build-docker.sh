c633e6988e98836aab8abf6737ccbd8b4dd8169b
BioHazzardt <biohazzardt@gmail.com>
2018-10-03 19:51:43 +0200

    Upd

--- contrib/build-docker.sh
+++ contrib/build-docker.sh
@@ -9,8 +9,7 @@ cd /${COIN}
 ./autogen.sh
 ./configure
 make
-strip ./${COIN}d
-strip ./${COIN}-cli
-mv ./${COIN}d /usr/local/bin/
-mv ./${COIN}-cli /usr/local/bin/
-rm -rf /${COIN}
+strip /${COIN}/src/${COIN}d
+strip /${COIN}/src/${COIN}-cli
+cp /${COIN}/src/${COIN}d /usr/local/bin/
+cp /${COIN}/src/${COIN}-cli /usr/local/bin/
